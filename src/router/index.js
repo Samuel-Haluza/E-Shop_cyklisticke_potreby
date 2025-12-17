9bd63b53bf2568e1e8899e5d21311660b1282a17
Samuel Haluza <samuel.haluza2@gmail.com>
2025-12-17 10:41:11 +0100

    Lazy loading

--- src/router/index.js
+++ src/router/index.js
@@ -2,20 +2,17 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import HomeView from '../views/HomeView.vue'
 import ProductsView from '../views/ProductsView.vue'
-import ProductDetailView from '../views/ProductDetailView.vue'
 import CartView from '../views/CartView.vue'
-import AboutView from '../views/AboutView.vue'
-import ContactView from '../views/ContactView.vue'
 
 const router = createRouter({
   history: createWebHistory('/E-Shop_cyklisticke_potreby/'),
   routes: [
     { path: '/', component: HomeView },
     { path: '/products', component: ProductsView },
-    { path: '/products/:id', component: ProductDetailView },
+    { path: '/products/:id', component: () => import('../views/ProductDetailView.vue') },
     { path: '/cart', component: CartView },
-    { path: '/about', component: AboutView },
-    { path: '/contact', component: ContactView }
+    { path: '/about', component: () => import('../views/AboutView.vue') },
+    { path: '/contact', component: () => import('../views/ContactView.vue') }
   ]
 })
 
